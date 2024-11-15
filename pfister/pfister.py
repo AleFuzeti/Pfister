493f0bb63b78e539f9ef0b928fcdf1b2adceee54
fuzeti <alebertipaglia@gmail.com>
2024-11-14 23:45:14 -0300

    v5 ids e txt com as ações

--- pfister/pfister.py
+++ pfister/pfister.py
@@ -51,30 +51,38 @@ class PyramidApp:
         self.selected_color = None
         self.dragging_item = None
         self.item_positions = {}
+        self.item_names = {}
 
         self.create_pyramid()
         self.create_palette()
 
     def create_pyramid(self):
         size = 40
-        # Cria a pirâmide de quadrados brancos
+        # Cria a pirâmide de quadrados brancos com identificadores
         for i in range(5):
             for j in range(i + 1):
                 x = 200 - (i * size / 2) + (j * size)
                 y = 50 + (i * size)
-                rect = self.canvas.create_rectangle( 
-                    x, y, x + size, y + size, fill=BACKGROUND_COLOR, tags=("square",)  
+                rect = self.canvas.create_rectangle(
+                    x, y, x + size, y + size, fill=BACKGROUND_COLOR, tags=("square",)
                 )
                 self.item_positions[rect] = (x, y)
                 self.canvas.tag_bind(rect, "<Button-1>", self.select_square)
+                
+                # Adiciona identificador maiusculo ao quadrado
+                identifier = str(i + 1) + chr(65 + j)
+                self.item_names[rect] = identifier
+                # self.canvas.create_text(
+                #     x + size / 2, y + size / 2, text=identifier, tags=("identifier",)
+                # )
                 
 
     def create_palette(self):
         # Cria a paleta de cores com a quantidade abaixo
         for i, color in enumerate(self.colors.values()):
-            x0, y0, x1, y1 = self.get_palette_coords(i)
+            x, y, = self.get_palette_coords(i)
             palette_rect = self.canvas.create_rectangle(
-                x0, y0, x1, y1, fill=color, outline="", tags=("palette",)
+                x, y, x + 40, y + 40, fill=color, outline="", tags=("palette",)
             )
             self.canvas.tag_bind(palette_rect, "<Button-1>", self.start_drag)
 
@@ -116,51 +124,65 @@ class PyramidApp:
         # Solta a cor e verifica se está sobre um quadrado
         if self.dragging_item:
             overlapping_items = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
-            placed = False
             for item in overlapping_items:
                 if "square" in self.canvas.gettags(item):
                     if self.canvas.itemcget(item, "fill") == BACKGROUND_COLOR:
                         # Preenche o quadrado de destino com a cor
                         self.canvas.itemconfig(item, fill=self.selected_color)
-                        placed = True
+                        color_name = list(self.colors.keys())[list(self.colors.values()).index(self.selected_color)]
+                        
+                        if "square" in self.canvas.gettags(self.dragging_item):
+                            self.canvas.itemconfig(self.dragging_item, fill=BACKGROUND_COLOR)
+                            # Retorna o quadrado original para a posição inicial
+                            x, y = self.item_positions[self.dragging_item]
+                            self.canvas.coords(self.dragging_item, x, y, x + 40, y + 40)
+                            
+                            # Registra ação remover
+                            id = self.item_names[self.dragging_item]    
+                            self.log_action("remove", color_name, id)
+                        # Registra a ação adicionar
+                        id = self.item_names[item]
+                        self.log_action("add", color_name, id)    
+                        
                     else:
-                        # Destino já pintado, retorna à posição original
-                        placed = False
-                    break
-            if placed:
-                # Torna o quadrado original branco
-                if "square" in self.canvas.gettags(self.dragging_item):
-                    self.canvas.itemconfig(self.dragging_item, fill=BACKGROUND_COLOR)
-                    # Retorna o quadrado original para a posição inicial
-                    x, y = self.item_positions[self.dragging_item]
-                    self.canvas.coords(self.dragging_item, x, y, x + 40, y + 40)
-            else:
-                # arraste não foi colocado, retorna à posição original
-                if "square" in self.canvas.gettags(self.dragging_item):
-                    # se o quadrado for arrastado para fora da piramide, torna branco e volta pra paleta
-                    self.canvas.itemconfig(self.dragging_item, fill=BACKGROUND_COLOR)
-                    # Retorna o quadrado original para a posição inicial
-                    x, y = self.item_positions[self.dragging_item]
-                    self.canvas.coords(self.dragging_item, x, y, x + 40, y + 40)
-                    
+                        if "current" in self.canvas.gettags(item):
+                            # pinta de branco o quadrado de origem
+                            self.canvas.itemconfig(self.dragging_item, fill=BACKGROUND_COLOR)
+                            # Registra ação remover
+                            color_name = list(self.colors.keys())[list(self.colors.values()).index(self.selected_color)]
+                            id = self.item_names[self.dragging_item]
+                            self.log_action("rem", color_name, id)
+                            # Destino já pintado, retorna à posição original
+                            x, y = self.item_positions[self.dragging_item]
+                            self.canvas.coords(self.dragging_item, x, y, x + 40, y + 40)
+                        else:
+                            # Retorna a cor para a paleta
+                            idx = list(self.colors.values()).index(self.selected_color)
+                            x, y = self.get_palette_coords(idx)
+                            self.canvas.coords(self.dragging_item, x, y, x + 40, y + 40)
+                            
+                    break                
             if "palette" in self.canvas.gettags(self.dragging_item):
                 # Devolve a cor para a paleta
                 idx = list(self.colors.values()).index(self.selected_color)
-                x0, y0, x1, y1 = self.get_palette_coords(idx)
-                self.canvas.coords(self.dragging_item, x0, y0, x1, y1)
+                x, y = self.get_palette_coords(idx)
+                self.canvas.coords(self.dragging_item, x, y, x + 40, y + 40)   
+
         
             self.dragging_item = None
         self.canvas.unbind("<Motion>")
         self.canvas.unbind("<ButtonRelease-1>")
     
+    def log_action(self, action, color_name, id):
+        with open("acoes.txt", "a") as file:
+            file.write(f"{action}\t{color_name}\t{id}\n")
+    
     def get_palette_coords(self, idx):
         row = idx // 6
         col = idx % 6
-        x0 = 380 + col * 50
-        y0 = 50 + row * 50
-        x1 = x0 + 40
-        y1 = y0 + 40
-        return x0, y0, x1, y1
+        x = 380 + col * 50
+        y = 50 + row * 50
+        return x, y
     
 if __name__ == "__main__":
     root = tk.Tk()
